f50e872e4ef63f69ef9d853adf68e0c6fe54aa53
hhhongso <52073383+hhhongso@users.noreply.github.com>
2019-07-12 20:52:48 +0900

    Commit0712
    Commit0712

--- Homeworks/src/kr/co/bit/MultiFor4.java
+++ Homeworks/src/kr/co/bit/MultiFor4.java
@@ -16,6 +16,34 @@ public class MultiFor4 {
 			}
 			n +=3; 
 			System.out.println();
-		}	
+		}
+		
+		/*
+		int h, i, j;
+		int n =2;
+		for(h = 1; h <= 3; h++) {
+			for(j = 1; j <=9 ; j++){
+				if(n <= 4) {
+					for(i = n; i <=4; i++){
+						System.out.print(i + " * " + j + " = " + i*j +"\t");
+					}						
+				}
+				else if (n >= 5 && n <= 7) {
+					for(i = n; i <= 7; i++){
+						System.out.print(i + " * " + j + " = " + i*j +"\t");	
+					}
+				} 
+				else if (n >=8 && n <= 9) {
+					for(i = n; i <=9; i++){
+						System.out.print(i + " * " + j + " = " + i*j +"\t");	
+					}
+				}				
+				System.out.println();	
+			}
+			n += 3;
+			System.out.println();
+	}
+	*/
+	
 	}
 }
